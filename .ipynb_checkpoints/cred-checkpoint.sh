fcc3e119617d40922b17ba8fc18dabfed81edd7b
sssvint <iinlustris@gmail.com>
2024-09-25 19:29:03 +0200

    fun with pandas - wip

--- .ipynb_checkpoints/cred-checkpoint.sh
+++ .ipynb_checkpoints/cred-checkpoint.sh
@@ -1,4 +1,11 @@
-/usr/bin/env bash
+#!/usr/bin/env bash
+
+pip install lxml
+pip install pandas
+pip install numpy
+pip install scipy
+pip install pingouin
+pip install seaborn
 
 # your git config information
 git config --global user.email "iinlustris@gmail.com"
